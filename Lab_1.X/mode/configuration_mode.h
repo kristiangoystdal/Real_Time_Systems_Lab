067cc02cdaff1566e08cfbe77141decfbdf6c879
Kristian Gøystdal <42619784+kristiangoystdal@users.noreply.github.com>
2024-12-06 15:35:55 +0000

    Delete Lab_1.X directory

--- Lab_1.X/mode/configuration_mode.h
+++
@@ -1,9 +0,0 @@
-#ifndef __CONFIGURATION_MODE_H
-#define __CONFIGURATION_MODE_H
-
-void configuration_mode_initialization();
-void configuration_mode_timer_handler();
-void configuration_mode_s1_handler();
-void configuration_mode_s2_handler();
-
-#endif
--- Lab_1.X/mode/normal_mode.h
+++
@@ -1,9 +0,0 @@
-#ifndef __NORMAL_MODE_H
-#define __NORMAL_MODE_H
-
-void normal_mode_initialization();
-void normal_mode_timer_handler();
-void normal_mode_s1_handler();
-void normal_mode_s2_handler();
-
-#endif
